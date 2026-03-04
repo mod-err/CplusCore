9d20db2298d3d0c3f3de9719a2fa27476162c88f
wzd <15027730753@163.com>
2026-03-04 20:48:04 +0800

    :2026-3-4笔记本提交

--- CplusCore/CplusCore.cpp
+++ CplusCore/CplusCore.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+﻿/*#include <iostream>
 using namespace std;
 
 int g_a = 10;
@@ -39,4 +39,135 @@ int main() {
 
 	system("pause");
 	return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+int* func() {
+	int a = 10;
+	return &a;
 }
+
+int main() {
+	int* p = func();
+	cout << *p << endl;
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+int* func1() {
+	int* p = new int(10);
+	return p;
+}
+void func2() {
+	int* arr = new int[10];
+	for (int i = 0; i < 10; i++) {
+		arr[i] = i + 100;//等价于*(arr + i) = i + 100;
+		cout << arr[i] << endl;
+	}
+	delete[] arr;
+}
+
+int main() {
+	int* p = func1();
+	cout << *p << endl;
+	delete p;
+
+	func2();
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+int* func1() {
+	int* p = new int(10);
+	return p;
+}
+void func2() {
+	int* arr = new int[10];
+	for (int i = 0; i < 10; i++) {
+		arr[i] = i + 100;//等价于*(arr + i) = i + 100;
+		cout << arr[i] << endl;
+	}
+	delete[] arr;
+}
+
+int main() {
+	int* p = func1();
+	cout << *p << endl;
+	delete p;
+
+	func2();
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+void func1(int a, int b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void func2(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+void func3(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int main() {
+	int a = 10;
+	int b = 20;
+	func1(a, b);
+	cout << "原值：" << a << " " << b << endl;
+	func2(&a, &b);
+	cout << "地址传递：\t" << a << " " << b << endl;
+	func2(&a, &b);
+
+	func3(a, b);
+	cout << "引用传递：\t" << a << " " << b << endl;
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+int& func1() {
+	int a = 10;
+	return a;
+}
+int& func2(){
+	static int a = 10;
+	return a;
+}
+
+int main() {
+	int& ref1 = func1();
+	cout << ref1 << endl;
+	cout << ref1 << endl;
+	
+	int& ref2 = func2();
+	cout << ref2 << endl;
+	cout << ref2 << endl;
+
+	func2() = 1000;
+	cout << ref2 << endl;
+	cout << ref2 << endl;
+}*/
+
+
+/*int* func3() {
+	int a = 10;
+	return &a;
+}
+int main() {
+	int* p = func3();
+	cout << p << endl;
+	//	cout << &p << endl;
+	cout << *p << endl;
+}*/
