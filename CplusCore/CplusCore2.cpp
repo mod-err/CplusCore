6e3104b018308ae921e64220e1cb908b568ca57a
wzd <15027730753@163.com>
2026-03-10 23:19:37 +0800

    2026-3-10台式机提交

--- CplusCore/CplusCore2.cpp
+++ CplusCore/CplusCore2.cpp
@@ -62,7 +62,7 @@ int main() {
 	g.visit();
 }*/
 
-#include <iostream>
+/*#include <iostream>
 #include <string>
 using namespace std;
 
@@ -76,7 +76,6 @@ public:
 	void visit();
 	Building* building;
 };
-
 class Building {
 	friend void goodGay::visit();
 public:
@@ -93,9 +92,88 @@ void goodGay::visit() {
 	cout << "好基友正在访问：" << building->mSittingroom << endl;
 	cout << "好基友正在访问：" << building->mBedroom << endl;
 }
-
-
 int main() {
 	goodGay g;
 	g.visit();
+}*/
+
+/*#include <iostream>
+#include <string>
+using namespace std;
+class Person {
+public:
+	//Person operator+(Person& person) {
+	//	Person temp;
+	//	temp.mA = this->mA + person.mA;
+	//	temp.mB = this->mB + person.mB;
+	//	return temp;
+	//}
+	int mA;
+	int mB;
+};
+
+Person operator+(Person& p1, Person& p2) {
+	Person temp;
+	temp.mA = p1.mA + p2.mA;
+	temp.mB = p1.mB + p2.mB;
+	return temp;
+}
+Person operator+(Person& p1, int num) {
+	Person temp;
+	temp.mA = p1.mA + num;
+	temp.mB = p1.mB + num;
+	return temp;
+}
+int main() {
+	Person p1;
+	p1.mA = 10;
+	p1.mB = 20;
+	Person p2;
+	p2.mA = 30;
+	p2.mB = 40;
+	Person p3;
+
+	//p3 = p1 + p2; 
+	//p3 = p1.operator+(p2);
+	p3 = p1 + 10;
+	cout << p3.mA << " " << p3.mB << endl;
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+class Person {
+public:
+	void operator<<(ostream& cout) {
+		cout << this->mA << " " << this->mB << endl;
+	}
+	int mA, mB;
+};
+
+int main() {
+	Person p;
+	p.mA = 10;
+	p.mB = 20;
+	//p.operator<<(cout);
+	p << cout;
+}*/
+#include <iostream>
+using namespace std;
+
+class Person {
+	friend ostream& operator<<(ostream& cout, Person& person);
+public:
+	Person() : mA(10), mB(20){
+	}
+private:
+	int mA, mB;
+};
+ostream& operator<<(ostream& cout, Person& person) {
+	cout << person.mA << " " << person.mB << endl;
+	return cout;
+}
+int main() {
+	Person p;
+	//operator<<(cout, p);
+	cout << p << endl;
 }
