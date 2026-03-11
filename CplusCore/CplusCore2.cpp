ccde745ca7b641574b91afe05408f5e998c1e5eb
wzd <15027730753@163.com>
2026-03-11 23:15:18 +0800

    2026-3-11台式机提交

--- CplusCore/CplusCore2.cpp
+++ CplusCore/CplusCore2.cpp
@@ -157,7 +157,8 @@ int main() {
 	//p.operator<<(cout);
 	p << cout;
 }*/
-#include <iostream>
+
+/*#include <iostream>
 using namespace std;
 
 class Person {
@@ -176,4 +177,94 @@ int main() {
 	Person p;
 	//operator<<(cout, p);
 	cout << p << endl;
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+class MyInt {
+	friend ostream& operator<<(ostream& cout, const MyInt& myInt);
+public:
+	MyInt() : mint(0) {}
+	//前置自增
+	MyInt& operator++() {
+		++mint;
+		return *this;
+	}
+	//后置自增
+	MyInt operator++(int) {
+		MyInt temp = *this;
+		mint++;
+		return temp;
+	}
+private:
+	int mint;
+};
+ostream& operator<<(ostream& cout, const MyInt& myInt) {
+	cout << myInt.mint;
+	return cout;
+}
+int main() {
+	MyInt m;
+	//m.operator++();
+	cout << ++(++m) << endl;
+	cout << m << endl;
+
+	cout << m++ << endl;
+	cout << m << endl;
+}*/
+
+/*#include <iostream>
+using namespace std;
+class Person {
+public:
+	Person(int age) {
+		mage = new int(age);
+	}
+	~Person() {
+		if (mage != NULL) { delete mage; mage = NULL; }
+	}
+	Person& operator=(Person& person) {
+		if (mage != NULL) { delete mage; mage = NULL; }
+		mage = new int(*person.mage);
+		return *this;
+	}
+	int* mage;
+};
+int main() {
+	Person p1(10);
+	Person p2(20);
+	Person p3(30);
+	//p2.operator=(p1);
+	p3 = p2 = p1;
+	cout << *p1.mage << endl;
+	cout << *p2.mage << endl;
+	cout << *p3.mage << endl;
+}*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Person {
+public:
+	Person(string name, int age) :mName(name), mAge(age) {}
+	bool operator==(Person& person) {
+		if (this->mAge == person.mAge && this->mName == person.mName)
+			return true;
+	}
+	bool operator!=(Person& person) {
+		if (this->mAge != person.mAge && this->mName != person.mName)
+			return false;
+	}
+	string mName;
+	int mAge;
+};
+
+int main() {
+	Person p1("张三", 19);
+	Person p2("张三", 19);
+	//if(p2.operator==(p1)) cout << "相同" << endl;
+	if(p1 == p2) cout << "相同" << endl;
+	else if (p1 != p2) cout << "不相同" << endl;
+
 }
